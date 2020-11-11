6876f459055e9ce90c1d2508f6b1fb380183901f
tracyjordan <42917505+tracyjordan@users.noreply.github.com>
2020-11-11 05:48:45 +0100

    Add index.html to purge array

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: ['./src/**/*.tsx'],
+  purge: ['./src/**/*.tsx', './public/index.html'],
   theme: {
     extend: {},
   },
